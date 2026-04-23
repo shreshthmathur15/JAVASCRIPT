a2c4be82d388c3233870c9d6f139376d33f1da18
Shreshth Mathur <shreshthmathur15@gmail.com>
2026-04-23 10:33:01 +0530

    updated

--- Loops/script4.js
+++ Loops/script4.js
@@ -1,3 +1,4 @@
+//
 // let sum = 0;
 // for (let i=1; i<= 5; i++) {
 //     sum = sum + i;
@@ -23,10 +24,10 @@
 
 // caluculate the sum of numbers 1 to 5
 
-let sum = 0;
+// let sum = 0;
 
-for (i = 1; i <= 5; i++) {
-    sum = sum + i;
-}
+// for (i = 1; i <= 5; i++) {
+//     sum = sum + i;
+// }
 
-console.log(sum , "is the sum");
+// console.log(sum , "is the sum");
